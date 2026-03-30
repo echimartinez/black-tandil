e724aaa7f0398ee7f83a903701437f46d8bf9bd5
echimartinez <echimartinez@gmail.com>
2026-03-30 13:51:50 -0300

    cambio precio camisaco para prueba de pago

--- src/data/products.ts
+++ src/data/products.ts
@@ -4,7 +4,7 @@ export const products: Product[] = [
    {
     id: 1,
     name: "Camisaco Oversize Marron",
-    price: 12000,
+    price: 1,
     image: "/camisacos.png",
     images: ["/camisacos.png", "/camisaco2.png"],
     category: "Camisacos",
